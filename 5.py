63510d4e68888af1eab587d546d78c3407ad971c
Benja <benjadf@outlook.com>
2018-07-03 23:17:09 -0400

    problema 5 resuelto

---
+++ 5.py
@@ -0,0 +1,19 @@
+def getDivisbleLimit(limit):
+    control = 1
+    v=0
+    while(1):
+        for i in range (1,limit):
+            if control%i != 0:
+                v=1
+        if v==0:
+            return control
+        control=control+1
+        v=0
+
+
+print(getDivisbleLimit(20))
+
+
+
+
+        
