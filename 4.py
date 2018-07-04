0fee3f61d9af4e06ee2f1613036ba166b19b7f10
Benja <benjadf@outlook.com>
2018-07-03 22:42:07 -0400

    Problema 4 resuelto, llamar a la funcion nNumberProduct con argumento de la cantidad de digitos para los productos. Para resolver el problema vale llamarla con argumento 3.

--- 4.py
+++ 4.py
@@ -1,4 +1,24 @@
 def isPalindrome(number):
-    str(number)
-    print("Soy un String: %d") %number
+    strnumber = str(number)
+    largo = len(strnumber)
+    if largo%2==0:
+        for i in range (0,largo):
+            if(strnumber[i]!=strnumber[largo-1]): 
+                return 0
+            else:
+                largo = largo-1
+                if(largo==0): 
+                    return 1
+    else: 
+        return 0
+        
+def nNumberProduct(number):
+    largestPalindrome = 0
+    for i in range (pow(10,number-1),pow(10,number)):
+        for j in range (pow(10,number-1),pow(10,number)):
+            if isPalindrome(i*j) == 1:
+                if (i*j>largestPalindrome):
+                     largestPalindrome=i*j
+    return largestPalindrome
 
+print nNumberProduct(3)
