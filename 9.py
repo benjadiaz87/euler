da90b659d4b8f61956721687a88f7387b8c72619
Benjamin Diaz <benjadf@outlook.com>
2018-07-07 12:20:15 -0400

    9 resuelto

---
+++ 9.py
@@ -0,0 +1,9 @@
+def pitagorianTriplet(number):
+    for i in range (1,number):
+        for j in range (i+1, number):
+                for k in range (j,number):
+                    if(i*i+j*j==k*k):
+                        if(i+j+k==1000):
+                            return i*j*k
+
+print(pitagorianTriplet(1000))
